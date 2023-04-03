d0166ce22a81560a8c1d04f8c483a66353331e59
Ivan Parkhomenko <ippeng54@gmail.com>
2023-04-03 22:07:33 +0300

    homework_1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,18 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture()
+def item_pc():
+    return Item("ПК", 15000, 2)
+
+
+def test_init(item_pc):
+    assert item_pc.name == "ПК"
+    assert item_pc.price == 15000
+    assert item_pc.quantity == 2
+
+
+def test_calculate_total_price(item_pc):
+    assert item_pc.calculate_total_price() == 30000
